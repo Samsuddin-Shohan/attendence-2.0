28655e96e1066bee0d5a6777656b3c8444685b37
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-06-27 19:37:39 +0600

    feat:crud completed

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,9 @@
 const User = require('../model/User');
 const error = require('../utils/error');
 const authService = require('../service/authService');
+const userService = require('../service/userService');
 
-const getUsers = async (req, res, next) => {
+const getUsers = async (_req, res, next) => {
   try {
     const users = await User.find();
     if (!users) {
@@ -31,10 +32,67 @@ const postUser = async (req, res, next) => {
     next(e);
   }
 };
-const getUserById = async (req, res, next) => {};
-const putUserById = async (req, res, next) => {};
-const patchUserById = async (req, res, next) => {};
-const deleteUserById = async (req, res, next) => {};
+const getUserById = async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const user = await userService.findUserByProperty('_id', userId);
+    if (!user) {
+      error('User not Found', 404);
+    }
+    res.status(200).json({ user });
+  } catch (e) {
+    next(e);
+  }
+};
+const deleteUserById = async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const user = await userService.findUserByProperty('_id', userId);
+    if (!user) {
+      error('User not Found', 404);
+    }
+    await user.remove();
+    res.status(202).json({ message: 'Deletion succeded' });
+  } catch (e) {
+    next(e);
+  }
+};
+const putUserById = async (req, res, next) => {
+  const { userId } = req.params;
+  const { name, roles, accountStatus, email } = req.body;
+  try {
+    const user = await userService.findUserByProperty('_id', userId);
+    if (!user) {
+      error('User not Found', 404);
+    }
+    const updatedUser = await userService.updateUser(userId, {
+      name,
+      roles,
+      accountStatus,
+      email,
+    });
+    res.status(200).json({ updatedUser });
+  } catch (e) {
+    next(e);
+  }
+};
+const patchUserById = async (req, res, next) => {
+  const { userId } = req.params;
+  const { name, roles, accountStatus } = req.body;
+  try {
+    const user = await userService.findUserByProperty('_id', userId);
+    if (!user) {
+      error('User not Found', 404);
+    }
+    user.name = name ?? user.name;
+    user.roles = roles ?? user.roles;
+    user.accountStatus = accountStatus ?? user.accountStatus;
+    await user.save();
+    res.status(200).json({ updatedUser: user });
+  } catch (e) {
+    next(e);
+  }
+};
 
 module.exports = {
   getUsers,
--- service/userService.js
+++ service/userService.js
@@ -1,5 +1,6 @@
 const User = require('../model/User');
 const { findById, findOne } = require('mongoose');
+const error = require('../utils/error');
 
 async function findUserByProperty(key, value) {
   let user;
@@ -13,7 +14,24 @@ async function createNewUser({ name, email, password, roles, accountStatus }) {
   const user = new User({ email, password, name, roles, accountStatus });
   return user.save();
 }
+async function updateUser(userId, data) {
+  try {
+    const user = await findUserByProperty('email', data.email);
+    if (user) {
+      error('email already in use', 404);
+    }
+    const updatedUser = await User.findOneAndUpdate(
+      userId,
+      { ...data },
+      { new: true }
+    );
+    return updatedUser;
+  } catch (e) {
+    next(e);
+  }
+}
 module.exports = {
   findUserByProperty,
   createNewUser,
+  updateUser,
 };
